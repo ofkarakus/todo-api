e0b8a1c647d165ddf530f2ac0c42809ae2132e42
Omer Karakus <omerfarukkarakus01@gmail.com>
2023-05-04 22:46:51 +0100

    set CORS up for requests

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const app = express();
 // Set up CORS
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
   res.header(
     "Access-Control-Allow-Headers",
